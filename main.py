2cd4202350fe0cc0d5cbf0941e45c06c58428150
abbyfewel <akfewel@outlook.com>
2025-01-21 11:17:12 -0800

    added comments

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello, world!!")
+print("Hello, world!!") # this is a comment for a single line
+'''
+and this is a comment 
+over multiple lines
+using single quotation marks
+
+
+
+:)
+
+
+
+
+'''
